fb2128e93a41eecede7417d3d172c38b5bc7ce27
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-04-26 19:54:35 +0200

    bug fix register function

--- js/api/call.js
+++ js/api/call.js
@@ -4,12 +4,13 @@ export async function fetchData(BASE_URL, endpoint, options = {}) {
   try {
     const response = await fetch(`${BASE_URL}${endpoint}`, options);
     const data = await response.json();
-    return data;
+
     if (response.status === 201) {
       displayMessage();
     } else {
       displayMessage("warning", data.errors[0].message);
     }
+    return data;
   } catch (error) {
     displayMessage("warning", error);
   }
