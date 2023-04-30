a953f98e2815beb332d141f7fddf74982bf89274
anasommer <68922341+anasommer@users.noreply.github.com>
2023-04-30 15:40:39 +0200

    add form interactivity

--- js/posts/createNewPost.js
+++ js/posts/createNewPost.js
@@ -1,4 +1,16 @@
-export function createNewPost() {
-  const createBtn = document.querySelector("#newPost");
-  createBtn.addEventListener("click", () => {});
-}
+const newPostForm = document.querySelector("#newPostForm");
+
+newPostForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const newPost = Object.fromEntries(formData.entries());
+  console.log(newPost);
+});
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const formData = new FormData(event.target);
+//   const profile = Object.fromEntries(formData.entries());
+
+//   login(profile);
+// });
