699bc269310770740eedf57bcdc9b8af5357c279
anasommer <68922341+anasommer@users.noreply.github.com>
2023-05-04 21:15:05 +0200

    move import statements up

--- js/posts/feed.js
+++ js/posts/feed.js
@@ -1,7 +1,8 @@
-const token = localStorage.getItem("accessToken");
-const feedContainer = document.querySelector("#feedContainer");
 import { getPosts } from "../api/getPosts.js";
 import spinner from "./spinner.js";
+
+const token = localStorage.getItem("accessToken");
+const feedContainer = document.querySelector("#feedContainer");
 
 async function showFeed(token, element) {
   token && element.append(spinner);
