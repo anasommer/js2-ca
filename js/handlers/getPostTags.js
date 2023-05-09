40714e88731dcef302f90a730e8e779cb0017010
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-05-09 15:55:58 +0200

    add filter function

--- js/handlers/getPostTags.js
+++ js/handlers/getPostTags.js
@@ -1,4 +1,29 @@
-export function getPostTags(event) {
+import { BASE_URL } from "../api/constants.js";
+import { renderPosts } from "../api/renderPosts.js";
+
+export async function getPostTags(event) {
   const tag = event.target.getAttribute("data-tags");
-  console.log(tag);
+  const feedContainer = document.querySelector("#feedContainer");
+
+  const token = localStorage.getItem("accessToken");
+
+  const endpoint = "posts/?_tag=";
+  const method = "GET";
+  const options = {
+    method,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${JSON.parse(token)}`,
+    },
+  };
+
+  feedContainer.innerHTML = "";
+
+  await fetch(`${BASE_URL}${endpoint}${tag}`, options).then((response) =>
+    response.json().then((posts) =>
+      posts.map((post) => {
+        renderPosts(post, feedContainer);
+      })
+    )
+  );
 }
